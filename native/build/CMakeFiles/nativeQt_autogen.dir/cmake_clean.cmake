e1fb6179c458ad3f1727987fb7e4df94bc9085a9
wathinst <wxz@xkzhineng.com>
2022-06-17 11:09:13 +0800

    误删忽略文件恢复

--- native/build/CMakeFiles/nativeQt_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/nativeQt_autogen"
-  "nativeQt_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/nativeQt_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
