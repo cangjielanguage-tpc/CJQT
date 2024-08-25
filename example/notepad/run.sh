ce70893084c6c912b35ff792b8ed082472627aa9
wathinst <wxz@xkzhineng.com>
2024-08-25 23:01:30 +0800

    支持aarch64

--- example/notepad/run.sh
+++ example/notepad/run.sh
@@ -2,10 +2,19 @@
 
 CUR_DIR=$(cd $(dirname $0) && pwd)
 
-export LD_LIBRARY_PATH=$CUR_DIR/../../native/lib:${LD_LIBRARY_PATH}
+GET_ARCH=`arch`
+if [[ $GET_ARCH =~ "x86_64" ]];then
+    QT_PLATFORM_URL=$CUR_DIR/../../native/lib/x86_64
+elif [[ $GET_ARCH =~ "aarch64" ]];then
+    QT_PLATFORM_URL=$CUR_DIR/../../native/lib/aarch64
+else
+    QT_PLATFORM_URL=$CUR_DIR/../../native/lib
+fi
+
+export LD_LIBRARY_PATH=$QT_PLATFORM_URL:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=$CUR_DIR/build/cjqt:${LD_LIBRARY_PATH}
 
-export QT_QPA_PLATFORM_PLUGIN_PATH=$CUR_DIR/../../native/lib/platforms
+export QT_QPA_PLATFORM_PLUGIN_PATH=$QT_PLATFORM_URL/platforms
 
 # native build
 sh $CUR_DIR/../../build.sh
--- native/make.sh
+++ native/make.sh
@@ -12,4 +12,4 @@ cd $BUILD_PATH
 cmake ..
 make
 
-cp $NATIVE_PATH/build/libnativeQt.so $NATIVE_PATH/lib/libnativeQt.so
+cp $NATIVE_PATH/build/libnativeQt.so $NATIVE_PATH/lib/aarch64/libnativeQt.so
