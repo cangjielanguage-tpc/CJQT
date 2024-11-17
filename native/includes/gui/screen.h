646c2ee74abb60ac78890609c190419cc4d0a43d
zhoulibo <zhoulibo@honhand.com>
2024-11-17 14:10:39 +0800

    example\draw中增加椭圆绘制

---
+++ native/includes/gui/screen.h
@@ -0,0 +1,15 @@
+#ifndef __VALIDATOR_H_
+#define __VALIDATOR_H_
+#include <QScreen>
+#include "config.h"
+
+class CjScreen : public QScreen
+{
+    Q_OBJECT
+public:
+//    CjScreen(QObject *parent = nullptr) : QScreen(parent) {}
+
+public:
+};
+
+#endif
